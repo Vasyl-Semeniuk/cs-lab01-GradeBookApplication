d5a7c59465ff992ae88e02b895fb99f4d066d72f
Vasyl Semeniuk <Vasyl.Semeniuk1@microsoft.wsei.edu.pl>
2023-03-30 16:30:10 +0200

    12 error

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -43,5 +43,15 @@ namespace GradeBook.GradeBooks
                 return 'F';
             }
         }
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+
+            base.CalculateStudentStatistics(name);
+        }
     }
 }
